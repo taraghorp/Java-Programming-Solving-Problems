3a0f104de1a8d311d72ae51da3f5b99547ed9e62
taraghorp <taraghorp@gmail.com>
2019-07-31 20:40:40 -0400

    Part4 fixed

--- Week2/src/Part4.java
+++ Week2/src/Part4.java
@@ -18,12 +18,34 @@ public class Part4 {
         }
     }
     
+    public String mystery(String dna) {
+    	  int pos = dna.indexOf("T");
+    	  int count = 0;
+    	  int startPos = 0;
+    	  String newDna = "";
+    	  if (pos == -1) {
+    	    return dna;
+    	  }
+    	  while (count < 3) {
+    	    count += 1;
+    	    newDna = newDna + dna.substring(startPos,pos);
+    	    startPos = pos+1;
+    	    pos = dna.indexOf("T", startPos);
+    	    if (pos == -1) {
+    	      break;
+    	    }
+    	  }
+    	  newDna = newDna + dna.substring(startPos);
+    	  return newDna;
+    	}
+    
     public void testUrl() {
         printUrls("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
     }
     
-    public static void main() {
+    public static void main(String[] args) {
         Part4 url = new Part4();
         url.testUrl();
+        System.out.println(url.mystery("ABCTDEFTHIJTSDTASD"));
     }
 }
