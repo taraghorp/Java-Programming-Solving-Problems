3ec63a7a6634a95dc68cca08ad62fffc65c6eb8d
taraghorp <taraghorp@gmail.com>
2019-08-04 21:06:18 -0400

    First Commit

---
+++ Week3/src/CountryExports.java
@@ -0,0 +1,60 @@
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class CountryExports {
+	public static void main(String[] args) {
+		CountryExports exports = new CountryExports();
+		exports.tester();
+	}
+	
+	public void tester() {
+		FileResource fr = new FileResource();
+		CSVParser parser = fr.getCSVParser();
+		String info = countryInfo(parser, "Germany");
+		parser = fr.getCSVParser();
+		listExportersTwoProducts(parser,"gold", "diamonds");
+		parser = fr.getCSVParser();
+		int number = numberOfExporters(parser,"gold");
+		parser = fr.getCSVParser();
+		bigExporters(parser, "$999,999,999");
+	}
+	
+	public String countryInfo(CSVParser parser, String country) {
+		String ret = "NOT FOUND";
+		for (CSVRecord line : parser) {
+			if (line.get("Country").equals(country)) {
+				ret = country + ": " + line.get("Exports") + ": " + line.get("Value (dollars)");
+			}
+		}
+		return ret;
+	}
+	
+	public void listExportersTwoProducts(CSVParser parser, String exportItem1, String exportItem2) {
+		for (CSVRecord line : parser) {
+			String exports = line.get("Exports");
+			if (exports.contains(exportItem1) && exports.contains(exportItem2)) {
+				System.out.println(line.get("Country"));
+			}
+		}
+	}
+	
+	public int numberOfExporters(CSVParser parser, String exportItem) {
+		int count = 0;	
+		for (CSVRecord line : parser) {
+			String exports = line.get("Exports");
+			if (exports.contains(exportItem))
+				count++;
+		}
+		return count;
+	}
+	
+	public void bigExporters(CSVParser parser, String amount) {
+		for (CSVRecord line : parser) {
+			String exportValue = line.get("Value (dollars)");
+			if (exportValue.length() > amount.length()) {
+				System.out.println(line.get("Country") + " " + line.get("Value (dollars)"));
+			}
+		}
+	}
+}
